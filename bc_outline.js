32876f51c107a9a79bcdb7e6995249e2bb0d588e
Justin Aybar <jaybar975@west-mec.org>
2019-03-28 14:33:51 -0700

    1st Commit Push
    Added my first name, last name, and todays to the HTML document and the Javascript files. Linked one CSS file to the HTML document. Linked the Javascript files to the HTML document. Added some variables and function to one of the Javascript files.

---
+++ bc_outline.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Tutorial Case
+
+   Author: Justin Aybar
+   Date: 3.28.19 
+
+   Filename: bc_outline.js
+
+
+   Function List
+   =============
+
+   makeOutline()
+      Generates the text of the table of contents
+      as a nested list
+
+   createList(source, TOCList, headings)
+      Creates an outline based on the source document,
+      list items are appended to TOCList,
+      the list items are based on the element names
+      specified in the headings array
+
+
+*/
+
+// Generate an outline based on h1 through h6 headings in the source document
+
+window.addEventListener("load", makeOutline);
+
+function makeOutline() {
+      // Location of the document outline =
+      var outline = document.getElementById("outline");
+
+      // Source document for the outline
+      var source = document.getElementById("doc");
+
+      var mainHeading = document.createElement("h1");
+      var outlineList = document.createElement("ol");
+      var headingText = document.createTextNode("Outline");
+}
