a0528c43ce275131b6c61904e1ad655fcb3b7d30
Justin Aybar <jaybar975@west-mec.org>
2019-03-29 14:34:01 -0700

    2nd Commit Push
    Added more variables, functions, and if else statements

--- bc_outline.js
+++ bc_outline.js
@@ -41,4 +41,49 @@ function makeOutline() {
       var mainHeading = document.createElement("h1");
       var outlineList = document.createElement("ol");
       var headingText = document.createTextNode("Outline");
+
+      mainHeading.appendChild(headingText);
+      outline.appendChild(mainHeading);
+      outline.appendChild(outlineList);
+
+      createList(source, outline);
+}
+
+function createList(source, outlineList) {
+      // Heading for the outline
+      var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+      // Previous level of the headings
+      var prevLevel = 0;
+      // /* Loop through all of the child nodes of source article until no child nodes are left 
+      for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+            var headLevel = headings.indexOf(n.nodeName);
+            if (headLevel !== -1) {
+                  var listElem = document.createElement("li");
+                  listElem.innerHTML = n.firstChild.nodeValue;
+                  if (headLevel === prevLevel) {
+                        // Append the list item to the current list
+                        outlineList.appendChild(listElem);
+                  } else if (headLevel > prevLevel) {
+                        // Start a new nested list
+                        var nestedList = document.createElement("ol");
+                        nestedList.appendChild(listElem);
+                        // Append nested list to last item in the current list 
+                        outlineList.lastChild.appendChild(nestedList);
+                        // Change the current list to the nested list
+                        outlineList = nestedList;
+                  } else {
+                        // Append the list to a higher list
+                        // Calculate the difference between the current and previous level
+                        var levelUp = prevLevel - headLevel;
+                        // Go up to the higher level
+                        for (var i = 1; i <= levelUp; i++) {
+                              outlineList = outlineList.parentNode.parentNode;
+                        }
+                        outlineList.appendChild(listElem);
+                  }
+
+                  // Update the preLevel
+                  prevLevel = headLevel;
+            }
+      }
 }
